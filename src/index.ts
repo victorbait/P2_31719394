a9cdd25a2d67c88806b862e87e030b0fc4bb602f
“Victor Perdomo” <victorbaitps@gmail.com>
2025-04-01 20:46:46 -0400

    Agregado variable process

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
-  });
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
